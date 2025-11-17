9ce101750b561942d29394d3ae9e1507038dbbf9
Marco Aurelio Siqueira <marco.gsf@hotmail.com>
2025-11-17 15:25:26 -0300

    Add templates

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -4,8 +4,17 @@ from django.shortcuts import render
 def index(request):
     return render(
         request,
-        'blog/pages/index.html',
-        {
-            'nome': 'Marco'
-        }
+        'blog/pages/index.html'
+    )
+
+def page(request):
+    return render(
+        request,
+        'blog/pages/page.html'
+    )
+
+def post(request):
+    return render(
+        request,
+        'blog/pages/post.html'
     )
